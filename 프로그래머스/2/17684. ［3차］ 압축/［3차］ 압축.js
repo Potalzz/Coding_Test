cef09cf7717b84efee83375ee81243a5fabf3d5a
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-06 12:23:39 +0900

    [level 2] Title: [3차] 압축, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -18,25 +18,26 @@ function solution(msg) {
     let char = words.pop()
     
     while (words.length > 0) {
-        index ++
-     
-        char += words.pop()
+        index ++ // 새로 추가할 사전 번호 업데이트
+        char += words.pop() // 비교할 문자 추가
            
+        // 사전에 포함된 문자일경우, 포함되지 않은 문자가 나올 때 까지 char 업데이트
         if (dict.has(char)) {
             while (words.length > 0) {
                 char += words.pop()
                 if (!dict.has(char)) break
             }
-            
         }
-        if (words.length == 0) {
-            break
-        }
-        dict.set(char, index)
-        answer.push(dict.get(char.slice(0,-1)))
-        char = char[char.length-1]
+        
+        
+        if (words.length == 0) break // char 업데이트를 끝내고, 남아있는 문자가 없으면 종료
+        
+        dict.set(char, index) // 사전에 문자 새로 추가
+        answer.push(dict.get(char.slice(0,-1))) // 마지막 문자 제외하고 사전에서 번호 추가
+        char = char[char.length-1] // char에 마지막 문자만 남겨두기
     }
-    
+
+    // while문 종료 후 남아있는 문자 처리 로직
     if (dict.get(char)) {
         answer.push(dict.get(char))    
     } else {
