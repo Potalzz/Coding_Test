871cf48df90dc815671abc3d4af97985371adb27
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-06 15:45:22 +0900

    [level 2] Title: 모음 사전, Time: 3.51 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,34 +1,32 @@
 /*
 사전에서 해당하는 문자가 몇번 째에 위치하는지 반환하여라.
 
-완전탐색 => DFS를 활용하여 해당 문자가 나오면 종료되게끔 설계.
+완전탐색 => DFS를 활용하여 사전을 채우고, 해당 단어의 index찾기.
 
-길이가 5미만일 경우 A를 채워줌.
-5x5 크기의 알파벳 Map을 만들어준다.
-2중 for문으로 Map을 순환한다.
-word의 길이에 따라 얼마나 반복할건지 결정.
+roop돌릴 문자열 모음 배열 생성
+빈 사전 생성.
+dfs 함수 정의.
+길이와 문자를 인수로 받음
 
+종료 조건 설정
+길이가 5를 달성할 시 종료
 
-
+aeiou를 roop돌리면서 dfs함수 호출
+dfs함수 호출될 때마다 문자열을 추가해주고, 길이 +1
 */
-function solution(target) {
+function solution(word) {
     const chars = ["A", "E", "I", "O", "U"]
-    
-    const dic = []
+    const dict = []
     
     function dfs(cur, len) {
-        if (len > 5) {
-            return
-        }
+        if (len > 5) return
+        dict.push(cur)
         
-        dic.push(cur)
-        
-        for (let i = 0; i < 5; i++) {
-            dfs(cur + chars[i], len + 1)
-        }
+        chars.forEach((el) => {
+            dfs(cur + el, len + 1)
+        })
     }
-    
     dfs("", 0)
     
-    return dic.indexOf(target)
+    return dict.indexOf(word)
 }
