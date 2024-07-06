0308b655340ac7ec171fd7abde3f25c2f5a3b94b
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-06 17:13:40 +0900

    [level 2] Title: 롤케이크 자르기, Time: 136.14 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,30 @@
+/*
+토핑 종류 수를 반으로 나누는 방법의 수를 반환
+
+종류 수가 홀수이면 0 반환
+map 객체 -> set ?
+값을 추가하는 건 Set이 좋고, 값을 하나 씩 빼면서 확인하는 건 Map이 좋다.
+
+*/
+
+function solution(topping) {
+    let count = 0
+    const typeMap = new Map()
+    const typeSet = new Set()
+    const various = new Set([...topping]).size
+    
+    topping.forEach((el) => typeMap.set(el, (typeMap.get(el) + 1 || 1)))
+    
+    topping.forEach((el) => {
+        typeSet.add(el)
+        typeMap.set(el, (typeMap.get(el) -1))
+        
+        if (typeMap.get(el) === 0) typeMap.delete(el)
+        
+        if (typeSet.size >= various / 2) {
+            if (typeSet.size === typeMap.size) count ++
+        }
+        
+    })    
+    return count;
+}
