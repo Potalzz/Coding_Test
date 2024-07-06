979e91e0646c476a63ca15aed3d1b5c1ee43746c
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-06 10:15:47 +0900

    [level 2] Title: 타겟 넘버, Time: 0.90 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,26 @@
+/*
++1과 -1 두가지로 나누어서 재귀함수로 반복.
+재귀함수에 들어갈 인수 (index, sum + 1 or -1 )
+
+종료조건
+inddex가 numbers.length와 동일하면 숫자의 합계를 확인하고 종료.
+*/
+
+function solution(numbers, target) {
+    let count = 0;
+    let length = numbers.length
+    function dfs (index, sum) {
+        if (index === length) {
+            if (sum === target) {
+                count += 1
+            }
+            return
+        }
+        
+        dfs(index + 1, sum + numbers[index])
+        dfs(index + 1, sum - numbers[index])
+    }
+    
+    dfs(0,0)
+    return count;
+}
