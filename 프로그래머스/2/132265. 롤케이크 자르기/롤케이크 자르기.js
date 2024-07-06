c344721087cd190ccf8221589d98bcb4a3fdb328
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-06 18:14:22 +0900

    [level 2] Title: 롤케이크 자르기, Time: 110.16 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,27 +1,29 @@
+/*
+토핑 종류 수를 반으로 나누는 방법의 수를 반환
+
+종류 수가 홀수이면 0 반환
+빼는 건 map 객체에서
+추가 하는 건 Set 객체에서
+값을 추가하는 건 Set이 좋고, 값을 하나 씩 빼면서 확인하는 건 Map이 좋다.
+*/
 function solution(topping) {
-    const a = new Set()
-    const b = {}
-
-    let answer = 0;
-    let check = 0
-
-    for (let i = 0; i < topping.length; i++) {        
-        if (b[topping[i]]) {
-            b[topping[i]]++
-        } else {
-            b[topping[i]] = 1
-            check++            
+    let count = 0
+    const typeMap = new Map()
+    const typeSet = new Set()
+    
+    topping.forEach((el) => typeMap.set(el, (typeMap.get(el) + 1 || 1)))
+    let various = typeMap.size
+    
+    topping.forEach((el) => {
+        typeSet.add(el)
+        typeMap.set(el, (typeMap.get(el) -1))
+        
+        if (typeMap.get(el) === 0) various --
+        
+        if (typeSet.size >= various / 2) {
+            if (typeSet.size === various) count ++
         }
-    }
-
-    for (let i = 0; i < topping.length; i++) {
-        a.add(topping[i])
-        b[topping[i]]--
-
-        if (!b[topping[i]]) check--
-        if (a.size === check) answer++
-    }
-
-
-    return answer;
+        
+    })    
+    return count;
 }
