80951481ad415abbf34fa745c2cf5b1853ded6a7
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-06 10:01:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 28.91 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,57 +1,53 @@
 /*
-문제
-좌측 상단 칸에서 우측 하단 칸까지의 최단 경로를 구해라.(0은 장애물)
-도달할 수 없으면 -1 return
+n x m 크기의 사각형에서 0,0시작점에서부터 n-1,m-1까지 최단 거리를 구하여라.
+=> bfs를 활용한 풀이.
 
-아이디어
-최단경로 ? BFS를 통한 풀어보자.
-현재 위치를 지정한다.
-동 -> 남 -> 서 -> 북 우선 순으로 이동한다.
-목표지점에 도착하면 현재까지 이동한 칸과 그동안의 값 중 낮은 값으로 저장.
+n과 m의 크기를 정의해준다.
+x와 y의 이동방향을 담은 변수를 만들어준다.
+maps의 크기와 동일한 방문 여부 확인 사각형을 만든다.
+초기값을 할당해준다. (시작점, queue)
+
+queue가 빌 때 까지 while문을 반복해준다.
+queue에 담긴 칸에대한 정보를 변수로 할당해준다. (해당 칸에 대한 정보를 가지고 동작)
+
+종료조건(n-1, m-1에 도착)을 할당해준다.
+
+현재의 칸에서 상하좌우 로 이동한다.
+칸에 이동할 수 있을 경우 방문 처리하고, 해당 칸에 대한 정보를 queue에 담는다.
+while문 처음으로 돌아가서 queue에 담긴 해당 칸의 정보를 shift해서 해당 칸으로부터 다시 이동을 한다.
+
+queue가 모두 비었을 경우, 모든 칸을 이동할 동안 종료조건(도착지점에 도달)을 만족하지 못했으므로
+-1(불가능)을 반환한다.
 */
+
 function solution(maps) {
- const dx =[-1,0,1,0];
- const dy =[0,1,0,-1];
- 
-
- const visited = Array.from({length:maps.length},()=> new Array(maps[0].length).fill(false),);
-  //게임맵과 똑같은 5x5지만 값은 0로 채워진 배열 을 생성합니다. 
- const dist = Array.from({length:maps.length},()=> new Array(maps[0].length).fill(0),);
-  const q =[]; 
-  q.push([0,0]);
-  visited[0][0]=true;
-  //도착지의 최단거리는 1입니다.
-  dist[0][0]=1;
-  
-  while(q.length){ 
-  const[curX,curY]=q.shift(); 
-  
-
-  for(let i =0; i<4; i++){ 
-  const x= curX+dx[i];   
-  const y= curY+dy[i];
-  
-
-  if(x>-1&&x<maps.length&&y>-1&&y<maps[0].length){
-  //처음 방문했을때 -> visited[x][y]이 false 일 때 
-    if(maps[x][y]===1&&!visited[x][y]){  
-     q.push([x,y]);
-        visited[x][y]=true;
+    const n = maps.length
+    const m = maps[0].length
+    
+    const dx = [0,1,0,-1]
+    const dy = [1,0,-1,0]
+    
+    const visited = Array.from({ length : n }, () => Array(m).fill(false))
+    visited[0][0] = true
+    
+    const queue = [[0, 0, 1]]
+    
+    while (queue.length > 0) {
+        const [x, y, distance] = queue.shift()
         
-        //먼저 탐색하지 않았다면 
-        if(dist[x][y]===0 ){
-           dist[x][y]=dist[curX][curY]+1;
+        if (x === n - 1 && y === m - 1) return distance
+        
+        for (let i = 0; i < 4; i ++) {
+            const nx = x + dx[i]
+            const ny = y + dy[i]
+            
+            if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
+                maps[nx][ny] === 1 && !visited[nx][ny]) {
+                visited[nx][ny] = true
+                queue.push([nx, ny, distance + 1])
+            }
         }
-       
-    };
-   };
-   
-   
-  };
-  
-  }
-  //아예 탐색 성공 못했을 때는 -1 아닐때는 좌표최단거리 주기 
-   return dist[maps.length - 1][maps[0].length - 1]
-    ? dist[maps.length - 1][maps[0].length - 1]
-    : -1;
+    }
+    
+    return -1;
 }
