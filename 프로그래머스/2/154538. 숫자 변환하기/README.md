9ff2a4897947468e0a9bf14a976ae34044c22b10
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-16 15:37:18 +0900

    [level 2] Title: 숫자 변환하기, Time: 307.79 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/README.md
+++ 프로그래머스/2/154538. 숫자 변환하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 변환하기 - 154538 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154538) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 284.41 ms
+메모리: 72.1 MB, 시간: 307.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 16일 15:29:27
+2024년 07월 16일 15:37:14
 
 ### 문제 설명
 
--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -12,21 +12,33 @@
 */
 
 function solution(x, y, n) {
+    // 숫자 같은 경우 0 반환
     if(x === y) return 0
+    // dp테이블 생성
     const dp = {}
+    //dp테이블에 연산해서 나온 값을 index로 count횟수를 넣어준다.
     dp[x] = 0
+    
+    // 연산해서 나온 값들로 계속해서 연산 작업
     let datas = [x]
+    // 목표값인 y를 넘어가는 값들로 이루어질 때까지 진행
     while(datas.length) {
-        const newDatas = []
+        const temp = []
+        // 이전 연산의 결과값들로 다시 연산 진행
         for(const data of datas) {
+            // 연산한 값을 value에 해당
             for(const value of [data + n, data * 2, data * 3]) {
+                // y를 넘어가는 경우 패스
                 if (value > y || dp[value])  continue
+                // 목표값과 일치할 경우 이전 연산횟수에 1을 더해준 값 반환
                 if (value === y) return dp[data] + 1
+                // dp[값] = count횟수로 dp테이블에 저장
                 dp[value] = dp[data] + 1
-                newDatas.push(value)
+                // 연산해서 나온 값들을 data에 넣어주기 위해 temp에 임시 저장
+                temp.push(value)
             }
         }
-        datas = newDatas
+        datas = temp
     }
     return -1
 }
