d8d30225065b4298f302ebe1f4e366060b6f1c0b
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-19 11:22:35 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 13.20 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,73 +1,78 @@
-/*
-n x m 보드의 지워지는 블록 총 개수 반환
-최대 30 x 30
-
-블록을 오른쪽으로 90도 회전한 모양으로 바꿔 배열로 만든다.
-(블록이 터지면서 위에 있는 블록이 아래로 내려오는 성질과 배열의 성질이 일치하기 때문)
-
-블록 확인
-한 행씩 순회하면서 같은거 2개 연속이면 다음 행으로 가서 확인.
-모든 칸을 다 돌 때 까지 정보 기억하고 있음.
-맞는 경우 false로 바꿔줌
-
-블록 터뜨리는 로직
-filter로 false가 아닌 값들로만 재구성
-*/
 function solution(m, n, board) {
-    let newBoard = Array.from({length : n}, () => Array(m).fill(false))
-    let index = m  - 1
-    // array와 동일한 성질이 되도록 요소 재배치
-    for(let y = 0; y < m; y++) {
-        for (let x = 0; x < n; x++) {
-            newBoard[x][y] = board[index][x]
-        }
-        index --
-    }
-    
-    let count = 0
-    // 블록게임 작동 로직
-    while (true) {
-        let temp = []
-        // 터뜨릴 블록 찾기
-        for(let x = 0; x < newBoard.length -1; x ++) {
-            for(let y = 0; y < newBoard[x].length -1; y ++) {
-                let curr = newBoard[x][y]
-                if (newBoard[x][y+1] === curr &&
-                    newBoard[x+1][y] === curr &&
-                    newBoard[x+1][y+1] === curr) {
-                    temp.push([x,y])
+    // Convert board to a 2D array
+    let boardArray = board.map(row => row.split(''));
+
+    // Helper function to check and mark blocks for removal
+    function markRemovals() {
+        const toRemove = Array.from({ length: m }, () => Array(n).fill(false));
+        let found = false;
+
+        for (let y = 0; y < m - 1; y++) {
+            for (let x = 0; x < n - 1; x++) {
+                const block = boardArray[y][x];
+                if (block &&
+                    block === boardArray[y][x + 1] &&
+                    block === boardArray[y + 1][x] &&
+                    block === boardArray[y + 1][x + 1]) {
+                    toRemove[y][x] = toRemove[y][x + 1] = toRemove[y + 1][x] = toRemove[y + 1][x + 1] = true;
+                    found = true;
                 }
             }
         }
-        if(temp.length === 0) break
-        
-        // 터뜨릴 블록들 false로 값 바꾸기
-        
-        temp.forEach(([x,y]) => {
-            if(newBoard[x][y]) {
-                newBoard[x].splice(y, 1, false)
-                count ++
+
+        return found ? toRemove : null;
+    }
+
+    // Helper function to remove marked blocks and return the count of removed blocks
+    function removeBlocks(toRemove) {
+        let count = 0;
+        for (let y = 0; y < m; y++) {
+            for (let x = 0; x < n; x++) {
+                if (toRemove[y][x]) {
+                    boardArray[y][x] = '';
+                    count++;
+                }
             }
-            if(newBoard[x][y+1]) {
-                newBoard[x].splice(y+1, 1, false)
-                count ++
+        }
+        return count;
+    }
+
+    // Helper function to drop blocks after removals
+    function dropBlocks() {
+        for (let x = 0; x < n; x++) {
+            let emptyRow = m - 1;
+            for (let y = m - 1; y >= 0; y--) {
+                if (boardArray[y][x] !== '') {
+                    if (y !== emptyRow) {
+                        boardArray[emptyRow][x] = boardArray[y][x];
+                        boardArray[y][x] = '';
+                    }
+                    emptyRow--;
+                }
             }
-            if(newBoard[x+1][y]) {
-                newBoard[x+1].splice(y, 1, false)
-                count ++
-            }
-            if(newBoard[x+1][y+1]) {
-                newBoard[x+1].splice(y+1, 1, false)
-                count ++
-            }
-        })
-        
-        // 블록 삭제함으로써 위 블록이 아래로 자동으로 내려오도록, 개수 카운팅
-        for(let i = 0; i < newBoard.length; i ++) {
-            newBoard[i] = newBoard[i].filter((el) => el !== false)
         }
     }
-    
-    
-    return count;
+
+    let totalRemoved = 0;
+
+    while (true) {
+        const toRemove = markRemovals();
+        if (!toRemove) break;
+
+        totalRemoved += removeBlocks(toRemove);
+        dropBlocks();
+    }
+
+    return totalRemoved;
 }
+
+// 테스트 예제 실행
+const m1 = 4;
+const n1 = 5;
+const board1 = ["CCBDE", "AAADE", "AAABF", "CCBBF"];
+console.log(solution(m1, n1, board1)); // 14
+
+const m2 = 6;
+const n2 = 6;
+const board2 = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"];
+console.log(solution(m2, n2, board2)); // 15
