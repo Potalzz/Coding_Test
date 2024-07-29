5c6fa9b343f73fcffa591d8dc59771a3ea80979f
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-29 16:26:46 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.36 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -9,38 +9,43 @@ course에 있는 숫자들을 순회하면서 해당하는 조합들을 모두 a
 알파벳 사전 순으로 정렬되어 있으므로, "CA"처럼 뒤집어진 경우 고려할 필요 X 
 완전탐색으로 모든 조합 사전에 기록.
 
+
+백트래킹
+객체 생성
+조합만들기 생성
+
 */
 
 function solution(orders, course) {
-  const ordered = {};
-  const candidates = {};
-  const maxNum = Array(10 + 1).fill(0);
-  const createSet = (arr, start, len, foods) => {
-    if (len === 0) {
-      ordered[foods] = (ordered[foods] || 0) + 1;
-      if (ordered[foods] > 1) candidates[foods] = ordered[foods];
-      maxNum[foods.length] = Math.max(maxNum[foods.length], ordered[foods]);
-      return;
+    const ordered = {}
+    const candidates = {}
+    const maxOrderCount = Array(11).fill(0)
+    
+    const getCourseMenu = (order, start, len, foods) => {
+        if (len === 0) {
+            ordered[foods] = (ordered[foods] + 1) || 1
+            if (ordered[foods] > 1) {
+                candidates[foods] = ordered[foods]
+                maxOrderCount[foods.length] = Math.max(maxOrderCount[foods.length], ordered[foods])
+            }
+            return
+        }
+        for (start; start < order.length; start ++) {
+            getCourseMenu(order, start + 1, len - 1, foods + order[start])
+        }
+        
     }
-
-    for (let i = start; i < arr.length; i++) {
-      createSet(arr, i + 1, len - 1, foods + arr[i]);
-    }
-  };
-
-  orders.forEach((od) => {
-    // arr.sort는 기본적으로 사전식 배열
-    const sorted = od.split('').sort();
-    // 주문한 음식을 사전순으로 배열해서 문자열을 만든다.
-    // course에 있는 길이만 만들면 된다.
-    course.forEach((len) => {
-      createSet(sorted, 0, len, '');
-    });
-  });
-
-  const launched = Object.keys(candidates).filter(
-    (food) => maxNum[food.length] === candidates[food]
-  );
-
-  return launched.sort();
+    
+    orders.forEach(order => {
+        const sortedOrder = order.split("").sort()
+        course.forEach((len) => {
+            getCourseMenu(sortedOrder, 0, len, "")
+        })
+    })
+    const launched = Object.keys(candidates).filter((food) => {
+        if (maxOrderCount[food.length] === candidates[food]) return food
+        
+    })
+    
+    return launched.sort()
 }
