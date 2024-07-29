3b173da574b6fdf6eca29b250457e422bf0f36e0
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-07-29 15:49:00 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 5.47 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -6,65 +6,41 @@
 course에 있는 숫자들을 순회하면서 해당하는 조합들을 모두 answer에 넣는다.
 
 메뉴 조합 기록 로직
-알파벳 사전 순으로 정렬시키면 "CA"처럼 뒤집어진 경우 고려할 필요 X 
+알파벳 사전 순으로 정렬되어 있으므로, "CA"처럼 뒤집어진 경우 고려할 필요 X 
 완전탐색으로 모든 조합 사전에 기록.
 
 */
 
 function solution(orders, course) {
-    // 각 주문을 알파벳 순서로 정렬
-    orders = orders.map((order) => order.split("").sort().join(""));
+  const ordered = {};
+  const candidates = {};
+  const maxNum = Array(10 + 1).fill(0);
+  const createSet = (arr, start, len, foods) => {
+    if (len === 0) {
+      ordered[foods] = (ordered[foods] || 0) + 1;
+      if (ordered[foods] > 1) candidates[foods] = ordered[foods];
+      maxNum[foods.length] = Math.max(maxNum[foods.length], ordered[foods]);
+      return;
+    }
 
-    const getCombinations = (array, length) => {
-        const results = [];
-        if (length === 1) return array.map((value) => [value]);
+    for (let i = start; i < arr.length; i++) {
+      createSet(arr, i + 1, len - 1, foods + arr[i]);
+    }
+  };
 
-        array.forEach((currentValue, index, original) => {
-            const remaining = original.slice(index + 1);
-            const combinations = getCombinations(remaining, length - 1);
-            const attached = combinations.map((combination) => [currentValue, ...combination]);
-            results.push(...attached);
-        });
-
-        return results;
-    };
-
-    const menuDict = new Map();
-
-    orders.forEach((order) => {
-        course.forEach((length) => {
-            const combinations = getCombinations(order.split(""), length);
-            combinations.forEach((combination) => {
-                const key = combination.join("");
-                if (menuDict.has(key)) {
-                    menuDict.set(key, menuDict.get(key) + 1);
-                } else {
-                    menuDict.set(key, 1);
-                }
-            });
-        });
+  orders.forEach((od) => {
+    // arr.sort는 기본적으로 사전식 배열
+    const sorted = od.split('').sort();
+    // 주문한 음식을 사전순으로 배열해서 문자열을 만든다.
+    // course에 있는 길이만 만들면 된다.
+    course.forEach((len) => {
+      createSet(sorted, 0, len, '');
     });
+  });
 
-    const result = [];
+  const launched = Object.keys(candidates).filter(
+    (food) => maxNum[food.length] === candidates[food]
+  );
 
-    course.forEach((length) => {
-        const candidates = [];
-        let maxCount = 0;
-
-        menuDict.forEach((count, key) => {
-            if (key.length === length && count >= 2) {
-                if (count > maxCount) {
-                    candidates.length = 0;
-                    candidates.push(key);
-                    maxCount = count;
-                } else if (count === maxCount) {
-                    candidates.push(key);
-                }
-            }
-        });
-
-        result.push(...candidates);
-    });
-
-    return result.sort();
+  return launched.sort();
 }
