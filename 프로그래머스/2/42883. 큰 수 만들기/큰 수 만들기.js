f8c23543c287415b8a0568390ed1534bc35b1a22
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-08-09 17:38:00 +0900

    [level 2] Title: 큰 수 만들기, Time: 14.98 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,13 +1,22 @@
 function solution(number, k) {
-    let stk = []
-    for (let i = 0; i < number.length; i ++) {
-        while (k > 0 && stk.length > 0 && number[i] > stk[stk.length - 1]) {
-            k --
-            stk.pop()
+    let stack = []
+    for (let i = 0; i < number.length; i ++ ) {
+        const item = number[i]
+        if (stack.length === 0) {
+            stack.push(item)
+            continue
         }
-        stk.push(number[i])
+        while (number[i] > stack[stack.length-1]) {
+            stack.pop()
+            k --
+            if (k === 0) return stack.join("") + number.slice(i)
+            
+            if (stack.length === 0) break
+        }
+        stack.push(item)
+        
     }
     
-    stk.splice(stk.length-k,k)
-    return stk.join("")
+    
+    return stack.join("").slice(0, number.length - k)
 }
