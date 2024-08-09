451ae19eff151415b858c5e94e7946bab274f7ac
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-08-09 17:22:52 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 10.73 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,14 +1,16 @@
 /*
-재귀함수로 표현
-값이 하나가 될 때 까지 재귀적으로 계속해서 호출.
+계속해서 압축을 진행해서 압축이 가능해지거나 1이 되면 반환.
+
+필요한 함수
+1. 압축 가능 판별 함수
+2. 압축하는 함수
 */
 
 function solution(arr) {
-    
     const isUniform = (x, y, size) => {
         let value = arr[x][y]
-        for(let i = x; i < x + size; i ++) {
-            for(let j = y; j < y + size; j ++) {
+        for (let i = x; i < x + size; i ++) {
+            for (let j = y; j < y + size; j ++) {
                 if (arr[i][j] !== value) return false
             }
         }
@@ -17,16 +19,18 @@ function solution(arr) {
     
     const compress = (x, y, size) => {
         if (size === 1 || isUniform(x, y, size)) {
-            return arr[x][y] === 0 ? [1, 0] : [0, 1]
+            return arr[x][y] === 0 ? [1,0] : [0,1]
         }
-        let harfSize = size / 2
-        let leftTop = compress(x, y, harfSize)
-        let rightTop = compress(x, y + harfSize, harfSize)
-        let leftBottom = compress(x + harfSize, y, harfSize)
-        let rightBottom = compress(x + harfSize, y + harfSize, harfSize)
+        let halfSize = size / 2
+        let topLeft = compress(x, y, halfSize)
+        let topRight = compress(x, y + halfSize, halfSize)
+        let bottomLeft = compress(x + halfSize, y, halfSize)
+        let bottomRight = compress(x + halfSize, y + halfSize, halfSize)
         
-        return [leftTop[0] + rightTop[0] + leftBottom[0] + rightBottom[0],
-                leftTop[1] + rightTop[1] + leftBottom[1] + rightBottom[1]]
+        return [topLeft[0] + topRight[0] + bottomLeft[0] + bottomRight[0],
+               topLeft[1] + topRight[1] + bottomLeft[1] + bottomRight[1]]
+        
     }
+
     return compress(0,0,arr.length)
 }
