b333a4eccfa741737549e5b3bbe1d7a09b6febe6
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-08-12 13:51:13 +0900

    [level 2] Title: 미로 탈출, Time: 26.15 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
 
 ### 성능 요약
 
-메모리: 39 MB, 시간: 19.35 ms
+메모리: 39.3 MB, 시간: 26.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 12일 13:34:32
+2024년 08월 12일 13:50:56
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -6,53 +6,50 @@
 2. bfs를 통해서 레버까지의 최단 경로를 찾고, 소요 시간, 좌표를 저장한다.
 3. 레버부터 출구까지 최단 경로를 찾고, 레버까지의 소요 시간을 더해준다.
 */
-
 function solution(maps) {
-    const bfs = (x, y, goal) => {
-        // 방문 여부 판별
+    const bfs = (start, goal) => {
+        const [x, y] = start
         const visited = Array.from({length : maps.length}, () => Array(maps[0].length).fill(false))
-        // 이동 방향 정의
         const dx = [0, 1, 0, -1]
         const dy = [1, 0, -1, 0]
-        // 시작 지점 방문 처리
         visited[x][y] = true
-        // 시작지점 queue에 삽입 
         let queue = [[x, y, 0]]
-        // 다음 이동할 곳이 없을 때 까지 이동
+        
+        // 이동할 곳이 없을 때 까지 이동
         for (let [cx, cy, distance] of queue) {
-            // 방향 별로 이동
             for (let i = 0; i < 4; i ++) {
-                let nx = cx + dx[i] // 다음 x 좌표
-                let ny = cy + dy[i] // 다음 y 좌표
+                let nx = cx + dx[i] 
+                let ny = cy + dy[i] 
                 // 이동 가능여부 판별
                 if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length &&
                     maps[nx][ny] !== "X" && !visited[nx][ny]) {
                     // 목표지점 도달 시 좌표, 거리 반환
-                    if (maps[nx][ny] === goal) return [nx, ny, distance + 1]
-                    
+                    if (maps[nx][ny] === goal) return distance + 1 
                     visited[nx][ny] = true
                     queue.push([nx, ny, distance + 1])
                 }
             }
         }
-        // 목표 도달하지 못했을 경우 false 반환
-        return [false, false, false]
+        // 목표 도달하지 못했을 경우 null 반환
+        return null
     }
-    // 시작 좌표 찾기
-    let startX, startY
-    for (let i = 0; i < maps.length; i ++) {
-        for (let j = 0; j < maps[0].length; j ++) {
-            if (maps[i][j] === "S") {
-                startX = i
-                startY = j
+    // 시작 좌표 찾는 함수
+    const findPosition = (target) => {
+        for (let i = 0; i < maps.length; i ++) {
+            for (let j = 0; j < maps[0].length; j ++) {
+                if (maps[i][j] === target) {
+                    return [i,j]
+                }
             }
         }
+        return null
     }
     
-    const [x, y, distance] = bfs(startX, startY, "L")
-    if (x === false) return -1
-    const [x2, y2, distance2] = bfs(x, y, "E")
-    if (x2 === false) return -1
+    // 실행 로직
+    const lever = bfs(findPosition("S"), "L")
+    if (lever === null) return -1
+    const exit = bfs(findPosition("L"), "E")
+    if (exit === null) return -1
     
-    return distance + distance2
+    return lever + exit
 }
