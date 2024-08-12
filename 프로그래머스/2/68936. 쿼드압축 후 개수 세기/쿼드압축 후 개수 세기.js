6646356442efa13d3dc1358aa0cecbcabbba2526
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-08-12 14:13:50 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.59 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,11 +1,3 @@
-/*
-계속해서 압축을 진행해서 압축이 가능해지거나 1이 되면 반환.
-
-필요한 함수
-1. 압축 가능 판별 함수
-2. 압축하는 함수
-*/
-
 function solution(arr) {
     const isUniform = (x, y, size) => {
         let value = arr[x][y]
@@ -19,18 +11,19 @@ function solution(arr) {
     
     const compress = (x, y, size) => {
         if (size === 1 || isUniform(x, y, size)) {
-            return arr[x][y] === 0 ? [1,0] : [0,1]
+            return arr[x][y] === 0 ? [1, 0] : [0, 1]
         }
-        let halfSize = size / 2
-        let topLeft = compress(x, y, halfSize)
-        let topRight = compress(x, y + halfSize, halfSize)
-        let bottomLeft = compress(x + halfSize, y, halfSize)
-        let bottomRight = compress(x + halfSize, y + halfSize, halfSize)
+        const halfSize = size / 2
+        const topLeft = compress(x, y, halfSize)
+        const topRight = compress(x, y + halfSize, halfSize)
+        const bottomLeft = compress(x + halfSize, y, halfSize)
+        const bottomRight = compress(x + halfSize, y + halfSize, halfSize)
         
         return [topLeft[0] + topRight[0] + bottomLeft[0] + bottomRight[0],
-               topLeft[1] + topRight[1] + bottomLeft[1] + bottomRight[1]]
-        
+                topLeft[1] + topRight[1] + bottomLeft[1] + bottomRight[1]]
     }
-
+    
+    
+    
     return compress(0,0,arr.length)
 }
