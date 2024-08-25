935c2ef6375fcc6fad69e77e05a2818af1282636
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-08-25 16:28:08 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.24 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.js
@@ -1,33 +1,30 @@
 /*
-첫 번째 자리수는 k-1 / factorial(n-1)
-두 번째 자리수는 k = k % factorial(n-1) 에서 k를 factorial(n-1)로 나눠준 값.
-
+총 조합의 개수는 n!이다.
+각각 자리수는 (n-1)!마다 변함.
+k를 (n-1)!로 나누고 다음 자리수를 계산
 */
 
 function solution(n, k) {
-    const factorial = [1,1,2,6,24]
-    const calcFactorial = (n) => {
-        if (factorial[n]) {
-            return factorial[n]
-        }
-        else {
-            factorial[n] = calcFactorial(n-1) * n
-            return factorial[n]
-        }
+    let factorial = [1,1,2,6,24]
+    function makeFactorial(n) {
+        if (factorial[n]) return factorial[n]
+        factorial[n] = makeFactorial(n-1) * n
+        return factorial[n]
     }
-    calcFactorial(n) 
+    makeFactorial(n)
     let result = []
-    let numsArray = []
-    for (let i = 0; i < n; i ++) {
-        numsArray[i] = i + 1
+    let nums = []
+    for(let i = 1; i <= n; i ++) {
+        nums.push(i)
     }
     
     while (n > 0) {
-        result.push(numsArray.splice(Math.floor((k-1) / factorial[n-1]),1)[0])
+        let index = Math.floor((k-1) / factorial[n-1])
+        if (index === n) index --
+        result.push(nums.splice(index,1)[0])
         k = k % factorial[n-1]
         n --
     }
-    if(result[0] === null) result.shift()
     
     return result
 }
