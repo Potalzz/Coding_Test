14814af3c51eadf6eff463d20cee02e1fdc25b07
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-09-03 14:39:04 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 87.27 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -5,18 +5,10 @@
 
 할인율 10%, 20%, 30%, 40%
 
-할인을 너무 많이 진행하면, 가입 안하고 이모티콘 구입.
-이모티콘 가입자 수가 최대로 되면서 가장 낮은 할인율 설정.
-
-데이터 구조
-[이모티콘 가격 : 할인율] 형태의 table로 구성.
-
 유저의 수가 최대 100명, 이모티콘의 개수가 최대 7개이므로 완전탐색으로 해결.
-case 하나당 연산 횟수는 유저 수 X 이모티콘 개수
-이모티콘이 7가지, 할인율의 종류가 4가지 이므로 
-모든 경우의 수마다 비교
+이모티콘이 7가지, 할인율의 종류 4가지의 모든 조합을 생성하고, 조합마다 비교
 
-중복 가능, 순서 상관 O인 경우이므로 중복조합으로 이모티콘 할인율 설정.
+중복조합으로 이모티콘 할인율 설정하여 가입자 수, 판매 금액 비교하여 최대값 리턴
 */
 
 function solution(users, emoticons) {
@@ -24,51 +16,53 @@ function solution(users, emoticons) {
     return saleCombination(users, emoticons)
 }
 
-function salePrice(price, percent) {
-    return price * ((100 - percent) / 100)
+function salePrice(price, percentage) {
+    return price * ((100 - percentage) / 100)
 }
 
 function saleCombination(users, emoticons) {
     let result = [0, 0]
     const arr = [10, 20, 30, 40]
-    function dfs(cur, len) {
-        if (cur.length === len) {
-        const money = new Array(users.length).fill(0)
-            const temp = [0, 0]
+    function dfs(sales, len) {
+        if (sales.length === len) {
+        const buyingPrice = new Array(users.length).fill(0)
+            const tempResult = [0, 0]
             for (let i = 0; i < users.length; i ++) {
                 let limit = users[i][0]
                 for (let j = 0; j < len; j ++) {
-                    if (limit <= cur[j]) {
-                        money[i] += salePrice(emoticons[j], cur[j])
+                    if (limit <= sales[j]) {
+                        buyingPrice[i] += salePrice(emoticons[j], sales[j])
+                        if (buyingPrice[i] >= users[i][1]) {
+                            break
+                        }
                     }
                 }
-                if (money[i] >= users[i][1]) {
-                    money[i] = Infinity
+                if (buyingPrice[i] >= users[i][1]) {
+                    buyingPrice[i] = Infinity
                 }
             }
-            for (let price of money) {
+            for (let price of buyingPrice) {
                 if (price === Infinity) {
-                    temp[0] ++
+                    tempResult[0] ++
                 }
                 else {
-                    temp[1] += price
+                    tempResult[1] += price
                 }
             }
             
-            if (temp[0] > result[0]) {
-                result = temp
+            if (tempResult[0] > result[0]) {
+                result = tempResult
             }
-            else if (temp[0] === result[0] && temp[1] > result[1]) {
-                result = temp
+            else if (tempResult[0] === result[0] && tempResult[1] > result[1]) {
+                result = tempResult
             }
-            // console.log(money)
             return
         }
         
         for (let i = 0; i < arr.length; i ++) {
-            cur.push(arr[i])
-            dfs(cur, len)
-            cur.pop()
+            sales.push(arr[i])
+            dfs(sales, len)
+            sales.pop()
         }
     }
     dfs([], emoticons.length)
