b7edd89380dcf288968444775500a1cfe496b75b
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-09-20 09:46:41 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -4,6 +4,7 @@ adj리스트로 변환하고, bfs를 통해 해결
 */
 
 function solution(n, computers) {
+    // 주어진 데이터 인접 리스트로 변환
     const adj = {}
     for (let r = 0; r < n; r ++) {
         adj[r+1] = []
@@ -16,29 +17,26 @@ function solution(n, computers) {
     
     // BFS 탐색
     const visited = new Array(n + 1).fill(false)
-    let visitCount = 0
-    let netWorkCount = 1
-    let queue = adj[1]
-    visited[1] = true
-    visitCount ++
-    while (visitCount < n) {
+    const bfs = (s) => {
+        let queue = adj[s]
+        visited[s] = true
         while (queue.length > 0) {
             let v = queue.shift()
             if (!visited[v]) {
                 visited[v] = true
-                visitCount ++
                 queue.push(...adj[v])
-            }
-        }
-        // 새로운 대륙에서 시작점 찾기
-        for (let i = 1; i <= n; i ++) {
-            if (!visited[i]) {
-                queue.push(i)
-                netWorkCount ++
-                break
             }
         }
     }
     
-    return netWorkCount
+    // 새로운 대륙에서 시작점 찾기
+    let result = 0
+    for (let i = 1; i <= n; i ++) {
+        if (!visited[i]) {
+            result ++
+            bfs(i)
+        }
+    }
+    
+    return result
 }
