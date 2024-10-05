9d102f2f0e7751d78ef9cef9669ba143de591973
Potalzz <149217055+Potalzz@users.noreply.github.com>
2024-10-06 00:06:28 +0900

    [level 3] Title: 정수 삼각형, Time: 25.87 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,35 +1,22 @@
 /*
-동적 계획법 5단계
+동적 계획법 단계
 1. 하위 문제 정의
-2. 하위 문제 부분 해 구하기
-3. 해법과 하위 문제 연결
-4. 메모이제이션 or 재귀로 알고리즘 구현
-5. 원래 문제 풀기
+2. 해의 일부 추측
+3. 하위 문제와 해법 연결 (점화식 세우기)
+4. 코드 구현
+5. 원래 문제와 비교
 
-다음 row에서는 이전 column과 column + 1로만 이동 가능하다.
+맨 아래 숫자부터 위로 올라가면서 큰 수를 배정
 
-하위 문제
-다음 row에서의 최대값 구하기
 
 */
 
 function solution(triangle) {
-    for (let i = 0; i < triangle.length - 1; i ++) {
-        triangle[i+1][0] += triangle[i][0]
-        triangle[i+1][triangle[i+1].length -1] += triangle[i][triangle[i].length - 1]
-        for (let j = 1; j < triangle[i+1].length - 1; j ++) {
-            triangle[i+1][j] += Math.max(triangle[i][j-1], triangle[i][j])
+    for (let i = triangle.length - 2; i >= 0; i --) {
+        for (let j = 0; j < triangle[i].length; j ++) {
+            triangle[i][j] += Math.max(triangle[i+1][j], triangle[i+1][j+1])
         }
     }
     
-    let answer = 0
-    let height = triangle.length - 1
-    for (let i = 0; i < triangle.length; i ++) {
-        let value = triangle[height][i]
-        if (value > answer) {
-            answer = value
-        }
-    }
-    
-    return answer
+    return triangle[0][0]
 }
