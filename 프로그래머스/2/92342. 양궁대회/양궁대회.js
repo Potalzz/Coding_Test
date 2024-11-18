1dbfd90c6d143b09776fef06fd21dda62c1dc3e0
Penguin <149217055+Potalzz@users.noreply.github.com>
2024-11-18 21:08:59 +0900

    [level 2] Title: 양궁대회, Time: 0.49 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,7 +1,4 @@
 /*
-도전자(어피치)가 모든 화살을 쏘고, 전 우승자(라이언)이 모든 화살 발사.
-이전 우승자에게 디메리트 적용.
-
 우승자 선정 방식
 1. 같은 점수를 더 많이 맞춘 사람이 해당 점수를 가져감.(동률일 경우 도전자가)
     1.1 10점을 각각 2번씩 맞출 경우 도전자가 10점 획득
@@ -14,52 +11,70 @@
 시간 복잡도
 n이 10이하의 수이므로 시간적 제한은 없다고 봐도 무방함.
 
+풀이 방법
+0점부터 10점까지 점수를 순환하면서 모든 경우의 수를 반영한다.
 
+경우의 수는 총 세 가지 이다.
+1. 라이언이 점수 획득
+2. 어피치가 점수 획득
+3. 무승부
 
+문제에서 구하고자 하는 게임 결과는 라이언이 가장 큰 점수차로 승리한 상황이므로
+각 점수마다 라이언에게 가장 유리한 상황으로 반영한다.
+총 세 가지 경우의 수를 통해 살펴보자.
 
+1. 라이언이 점수 획득
+- 라이언이 이겼을 경우는 화살 1개 차이로 이긴 경우가 가장 유리하다
+
+2. 어피치가 점수 획득
+- 어피치가 이겼을 경우는 화살을 한 개도 사용하지 않은 경우가 가장 유리하다
+
+3. 무승부
+- 무승부는 화살 0개 사용으로 고정되어있다.
+
+각 점수마다 세 가지 상황을 반영하여 나올 수 있는 모든 결과를 재귀적으로 구한다.
+그리고 가장 큰 점수차를 기록한 경기는 게임의 점수판을 저장해둔 뒤, 마지막에 반환한다.
+
+우리가 구해야 할 부분은 점수차 이므로, 라이언과 어피치 각각의 점수를 저장하지 않고, 점수 차만 반영하면서 게임을 진행한다.
 */
 
 function solution(n, info) {
-    let answer = Array(11).fill(0)
-    let maxPoint = 0
+    let maxScoreBoard = Array(11).fill(0)
+    let maxScoreDifference = 0
     
-    
-    function findMaxPoint(challengerPoint, championPoint, usedShot, currentPoint, arr) {
-        if (usedShot > n) {
+    const findMaxScore = (scoreDifference, attemptCount, point, scoreBoard) => {        
+        if (attemptCount > n) {
             return
         }
         
-        if (currentPoint > 10) {
-            let diff = championPoint - challengerPoint
-            if (diff > maxPoint) {
-                arr[10] = n - usedShot
-                maxPoint = diff
-                answer = arr
+        if (point > 10) {
+            if (scoreDifference > maxScoreDifference) {
+                scoreBoard[10] = n - attemptCount
+                maxScoreBoard = scoreBoard
+                maxScoreDifference = scoreDifference
             }
             return
         }
         
-        let currentArr = [...arr]
-        currentArr[10 - currentPoint] = info[10 - currentPoint] + 1
-        
-        findMaxPoint(
-        challengerPoint,
-        championPoint + currentPoint,
-        usedShot + info[10 - currentPoint] + 1,
-        currentPoint + 1,
-        currentArr
-        )
-        
-        
-        if (info[10 - currentPoint] > 0) {
-            findMaxPoint(challengerPoint + currentPoint, championPoint, usedShot, currentPoint + 1, arr)
+        const index = 10 - point
+        // 라이언이 점수 획득
+        const updateBoard = [...scoreBoard]
+        updateBoard[index] = info[index] + 1
+        findMaxScore(scoreDifference + point, attemptCount + info[index] + 1, point + 1, updateBoard)
+        // 어피치가 점수 획득
+        if (info[index] > 0) {
+            findMaxScore(scoreDifference - point, attemptCount, point + 1, scoreBoard )    
         }
         else {
-            findMaxPoint(challengerPoint, championPoint, usedShot, currentPoint + 1, arr)
+            // 둘다 0점
+            findMaxScore(scoreDifference, attemptCount, point + 1, scoreBoard )
         }
     }
     
-    findMaxPoint(0,0,0,0,answer)
+    findMaxScore(0, 0, 0, maxScoreBoard)
     
-    return maxPoint <= 0 ? [-1] : answer
+    if (maxScoreDifference) {
+        return maxScoreBoard
+    }
+    return [-1]
 }
